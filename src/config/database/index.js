54355c9eeb0cdf2faeebfb83910573886d946bfd
NGUYỄN VĂN THUẬN <MY ASUS@NGUYENVANTHUAN-2002-QUANGNAM.THUAN22102002>
2022-07-13 23:35:45 +0700

    thuan dep trai

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose")
 async function connect() {
     try {
         await mongoose.connect('mongodb+srv://nguyenvanthuan:thuan22102002@cluster0.w0fje.mongodb.net/survey?retryWrites=true&w=majority');
-        console.log("kết nối database thành công!!!");
+        console.log("kết nối database thành công!");
     } catch (error) {
-        console.log("kết nối database thất bại!!!");
+        console.log("kết nối database thất bại!");
     }
 }
 
